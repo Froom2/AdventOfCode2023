553d15a28f6e24930998f300012ad30b4a5cf879
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2023-12-20 12:22:04 +0000

    correcting day 1 pt 1

--- src/main/scala/Day1/Day1.scala
+++ src/main/scala/Day1/Day1.scala
@@ -9,6 +9,38 @@ object Day1 {
   val input: String = Files.readString(Path.of("src/main/scala/Day1/Day1Input.txt"), StandardCharsets.UTF_8)
   val trimmedList: List[String] = input.split("\\n").map(_.trim).toList
 
+  def getNumbers(input: String): List[Int] = {
+    val numbers = List(0,1,2,3,4,5,6,7,8,9)
+
+    val numbersAsListOfStrings = numbers.map(_.toString)
+    val inputAsListOfStrings = input.toList.map(_.toString)
+
+    val res = inputAsListOfStrings.filter(numbersAsListOfStrings.contains(_))
+
+    res.map(_.toInt)
+  }
+
+  def getFirstAndLastNumbers(input: String): Int = {
+    val numbers = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    val numbersAsListOfStrings = numbers.map(_.toString)
+    val inputAsListOfStrings = input.toList.map(_.toString)
+
+    val res = inputAsListOfStrings.filter(numbersAsListOfStrings.contains(_))
+    val concatenated = res.head + res.last
+    val answer = concatenated.toInt
+
+    answer
+  }
+
+  def getDay1Answer(input:List[String]): Int = {
+
+    val firstAndLasts = input.map(getFirstAndLastNumbers)
+    println("tail: " + firstAndLasts.last + " ** " + firstAndLasts.length)
+    firstAndLasts.sum
+  }
+
 
 
 }
+
--- src/test/scala/Day1/Day1Spec.scala
+++ src/test/scala/Day1/Day1Spec.scala
@@ -11,5 +11,40 @@ class Day1Spec extends AnyFreeSpec {
         Day1.trimmedList.size must be(1000)
       }
     }
+    "getNumbers should pull the numbers out of the string" - {
+      "when there are no numbers" in {
+        Day1.getNumbers("abc") must be(List())
+      }
+      "when given single number in string" in {
+        Day1.getNumbers("1") must be(List(1))
+      }
+      "when given two numbers in string" in {
+        Day1.getNumbers("12") must be(List(12))
+      }
+      "when given two numbers in a mixed char string" in {
+        Day1.getNumbers("1rt2ab") must be(List(12))
+      }
+    }
+    "getFirstAndLastNumbers" - {
+      "should get only the number on it's own if there is only one number" in {
+        Day1.getFirstAndLastNumbers("4") must be(44)
+      }
+      "should get only the first and last number in the list" in {
+        Day1.getFirstAndLastNumbers("1234") must be(14)
+      }
+    }
+    "getDay1Answer" - {
+      "should get the answer for the day" in {
+
+        val testInput = List(
+          "1abc2", //12
+            "pqr3stu8vwx", //11
+            "a1b2c3d4e5f", //6
+            "treb7uchet" //77 =
+        )
+        Day1.getDay1Answer(testInput) must be(142)
+        Day1.getDay1Answer(Day1.trimmedList) must be(1000)
+      }
+    }
   }
 }
