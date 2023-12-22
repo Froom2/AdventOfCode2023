8bcf63fa7bfd8e069798fc8a4c6677f962479430
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2023-12-22 18:43:34 +0000

    correcting parsing

--- src/main/scala/Day2/Day2.scala
+++ src/main/scala/Day2/Day2.scala
@@ -14,10 +14,20 @@ object Day2 {
   case class CubeSet(blue: Int, red: Int, green: Int)
 
   def makeGame(string: String): Game = {
-    val split1 = string.split(": ")
-    val gameId = split1.head.substring(4).toInt
 
-    val sets = split1.last.split(";")
+    val test = "Game 91: 3 red, 1 green; 14 red, 2 blue, 2 green; 7 red, 3 blue; 1 blue, 6 red"
+    val split1 = string.split(": ")
+    val splitTest = test.split(": ").toList
+    splitTest.foreach(println)
+
+    val gameId = split1.head.substring(5).toInt
+    val gameIdTest = splitTest.head.substring(5).toInt
+    println(gameIdTest)
+
+    val sets = split1.last.split("; ")
+    val setsTest = splitTest.last.split(";")
+    setsTest.foreach(println)
+
     val cubeSets = sets.map(makeCubeSet).toList
     Game(gameId, cubeSets)
   }
@@ -40,6 +50,72 @@ object Day2 {
     val green = getCubeNumber("green")
 
     CubeSet(blue, red, green)
+  }
+
+  def gamepossible(bagCubes: CubeSet, game:Game): Boolean = {
+    val listOfSets = game.cubeSets
+    val setsPossible = listOfSets.map(set => {
+      val greenPossible = bagCubes.green >= set.green
+      val redPossible = bagCubes.red >= set.red
+      val bluePossible = bagCubes.blue >= set.blue
+
+      greenPossible & redPossible & bluePossible
+    })
+
+    !setsPossible.contains(false)
+  }
+
+  def getDay2Answer(input: List[String], bagCubes: CubeSet): Int = {
+    val listOfGames = input.map(makeGame)
+    val listOfGamesPossible = listOfGames.map(gamepossible(bagCubes, _))
+//    listOfGamesPossible.foreach(println)
+
+    println("**** game 91: " + listOfGames(90))
+
+    println(makeGame("Game 91: 3 red, 1 green; 14 red, 2 blue, 2 green; 7 red, 3 blue; 1 blue, 6 red"))
+
+    val filteredGamesPossible = listOfGames.filter(gamepossible(bagCubes, _))
+
+    val gamesNotPossible = listOfGames.filter(!gamepossible(bagCubes,_))
+
+    val redpossiblemax = filteredGamesPossible.map(g => {
+      val allRedInGame = g.cubeSets.map(
+        c => {
+          c.red
+        }
+      )
+      allRedInGame.max
+    })
+    println("&&&&& RED " + redpossiblemax.max)
+
+    val greenpossiblemax = filteredGamesPossible.map(g => {
+      val allGreenInGame = g.cubeSets.map(
+        c => {
+          c.green
+        }
+      )
+      allGreenInGame.max
+    })
+    println("&&&&& GREEN " + greenpossiblemax.max)
+
+    val bluepossiblemax = filteredGamesPossible.map(g => {
+      val allBlueInGame = g.cubeSets.map(
+        c => {
+          c.blue
+        }
+      )
+      allBlueInGame.max
+    })
+    println("&&&&& BLUE " + bluepossiblemax.max)
+
+    println("***IDs " + filteredGamesPossible.map(_.id))
+    println("***IDs length " + filteredGamesPossible.length)
+
+    println("&&& NOT POSSIBLE: " + gamesNotPossible.map(_.id))
+    println("***NOT POSSIBLE IDs length " + gamesNotPossible.length)
+
+    val sumOfIds = filteredGamesPossible.map(_.id).sum
+    sumOfIds
   }
 
 }
--- src/test/scala/Day2/Day2Spec.scala
+++ src/test/scala/Day2/Day2Spec.scala
@@ -10,27 +10,60 @@ class Day2Spec extends AnyFreeSpec {
   "On day 2" - {
     "it should read the input in correctly" - {
       "and have the right number of inputs" in {
-        println(Day2.trimmedList.head)
-        Day2.trimmedList.size must be(100)
+        println(trimmedList.head)
+        trimmedList.size must be(100)
       }
       "and turn it into a better structure" in {
-        val testData = List(
-          "Game1: 3 blue, 4 red;1 red, 2 green, 6 blue;2 green",
-          "Game2: 1 blue, 2 green;3 green, 4 blue, 1 red;1 green, 1 blue",
-          "Game3: 8 green, 6 blue, 20 red;5 blue, 4 red, 13 green;5 green, 1 red",
-          "Game4: 1 green, 3 red, 6 blue;3 green, 6 red;3 green, 15 blue, 14 red",
-          "Game5: 6 red, 1 blue, 3 green;2 blue, 1 red, 2 green"
-        )
 
-        Day2.makeCubeSet("3 blue, 4 red") must be(CubeSet(3,4,0))
-
-        Day2.makeGame("Game1: 3 blue, 4 red;1 red, 2 green, 6 blue;2 green") must be(
-          Game(1, List(CubeSet(3,4,0), CubeSet(6,1,2), CubeSet(0,0,2)))
+//        makeCubeSet("3 blue, 4 red") must be(CubeSet(3,4,0))
+//
+//        makeGame("Game 1: 3 blue, 4 red;1 red, 2 green, 6 blue;2 green") must be(
+//          Game(1, List(CubeSet(3,4,0), CubeSet(6,1,2), CubeSet(0,0,2)))
+//        )
+        makeGame("Game 91: 3 red, 1 green; 14 red, 2 blue, 2 green; 7 red, 3 blue; 1 blue, 6 red") must be(
+          Game(91, List(CubeSet(0,3,1),CubeSet(2,14,2),CubeSet(3,7,0),CubeSet(1,6,0)))
         )
       }
     }
 
-    "it should "
+    "it should tell if a game is possible" in {
+
+      val bagCubes = CubeSet(14, 12, 13)
+      val gameToTest = Game(1, List(CubeSet(3,4,0), CubeSet(6,1,2), CubeSet(0,0,2)))
+
+      gamepossible(bagCubes, gameToTest) must be(true)
+
+      val gameToTest2 = makeGame("Game 3: 8 green, 6 blue, 20 red;5 blue, 4 red, 13 green;5 green, 1 red")
+      gamepossible(bagCubes, gameToTest2) must be(false)
+
+      val gameToTest3 = Game(1, List(CubeSet(14,12,13)))
+      gamepossible(bagCubes, gameToTest3) must be(true)
+
+      val gameToTest4 = Game(1, List(CubeSet(14, 20, 13)))
+      gamepossible(bagCubes, gameToTest4) must be(false)
+
+      val gameToTest5 = Game(1, List(CubeSet(14, 12, 20)))
+      gamepossible(bagCubes, gameToTest5) must be(false)
+
+      val gameToTest6 = Game(1, List(CubeSet(20, 12, 13)))
+      gamepossible(bagCubes, gameToTest6) must be(false)
+    }
+
+    "it should get the Day 2 answer" in {
+      val testData = List(
+        "Game 1: 3 blue, 4 red;1 red, 2 green, 6 blue;2 green",
+        "Game 2: 1 blue, 2 green;3 green, 4 blue, 1 red;1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red;5 blue, 4 red, 13 green;5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue;3 green, 6 red;3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green;2 blue, 1 red, 2 green"
+      )
+      val bagCubes = CubeSet(14, 12, 13)
+
+      getDay2Answer(testData, bagCubes) must be(8)
+
+      getDay2Answer(trimmedList, bagCubes) must be(2076)
+
+    }
   }
 
 }
